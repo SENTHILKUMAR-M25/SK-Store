5f1307dbe05ed5105a97f79205f3bbb8eac65e4b
SENTHILKUMAR-M25 <senthilkumar393946@gmail.com>
2025-11-01 20:28:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Product from './Componets/Product.jsx'
+
+function App() {
+  console.log('senthil')
+  return (
+    <>
+    <Product />
+      </>
+  )
+}
+
+export default App
---
+++ src/Components/Pages/CardDraw.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardDraw() {
+  return (
+    <div>CardDraw</div>
+  )
+}
+
+export default CardDraw
